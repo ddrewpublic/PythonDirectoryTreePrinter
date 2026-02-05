f7481339bcfb2f35f9d2fd664c7a02b686734de5
Daniel Drew <github-account@drew.email>
2026-02-05 11:10:02 +0000

    Added collapsible Markdown output

--- src/python_directory_tree_printer/__init__.py
+++ src/python_directory_tree_printer/__init__.py
@@ -1,10 +1,11 @@
 # src/pytree_printer/__init__.py
 # comment: re-export main public API from the custom_logger module
-from .pytree_printer import print_tree, _get_package_version
+from .pytree_printer import print_tree, print_tree_md, _get_package_version
 
 # comment: public symbols for `from utilities_custom_logger import *`
 __all__ = [
     "print_tree",
+    "print_tree_md",
     "__version__"
 ]
 
